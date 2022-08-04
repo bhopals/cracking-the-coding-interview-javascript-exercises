01ecbb9940a50761f9bb78b3770220597c1f8ffc
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-08-04 08:48:44 -0400

    02. checkPermute

--- 01-arrays-and-strings/1.2 - Check Perm/checkPermute_ 1.js
+++ 01-arrays-and-strings/1.2 - Check Perm/checkPermute_ 1.js
@@ -3,6 +3,9 @@
  * Given two strings, write a method to decide if one is a permutation of the other.
  */
 function checkPermute(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
   return str1.split("").sort().join("") === str2.split("").sort().join("");
 }
 
