af1b9b3479f57a9d017803bf6523ccb3177ab0ea
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-08-04 08:29:09 -0400

    add questions

---
+++ 01-arrays-and-strings/1.4 - PalinPerm/palinPerm_.js
@@ -0,0 +1,12 @@
+/***
+ * Given a string, write a function to check if it is a permutation of a palindrome.
+ * A Palindrome is a word or phrase that is the same forwards and backwards.
+ * A Permutation is a rearrangement of letters. The palindrome does not
+ * need to be limited to just dictionary words.
+ *
+ */
+function palinPerm(str) {}
+
+/**Test*/
+console.log(palinPerm("Tact Coa"), "true");
+console.log(palinPerm("Tact boa"), "false");
